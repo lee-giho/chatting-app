b870f88fd672eaf871b1cfbdc6008b0da89997f2
giho <ff6022@naver.com>
2025-05-09 13:15:54 +0900

    프론트에서 닉네임 중복확인 요청 구현 / 회원가입 화면에서 검증을  통해 버튼 활성화

--- front-end/chatting_app/lib/screens/register_screen.dart
+++ front-end/chatting_app/lib/screens/register_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:chatting_app/utils/checkValidate.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
@@ -26,7 +27,19 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
+  // 값 입력 여부
+  bool isIdInput = false;
+  bool isNickNameInput = false;
+
+  // 중복확인 여부
   bool idNotDuplication = false;
+  bool nickNameNotDuplication = false;
+
+  // 상태값
+  bool isIdValid = false;
+  bool isPasswordValid = false;
+  bool isRePasswordValid = false;
+  bool isNickNameValid = false;
 
   @override
   void dispose() {
@@ -64,6 +77,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
         setState(() {
           idNotDuplication = !isDuplication;
+          isIdValid = !isDuplication;
         });
 
         if (idNotDuplication) {
@@ -80,6 +94,55 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text("아이디 중복 확인 실패"))
+        );
+      }
+    } catch (e) {
+      // 예외 처리
+      log(e.toString());
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("네트워크 오류: ${e.toString()}"))
+      );
+    }
+  }
+
+  // 닉네임 중복확인 요청 함수
+  Future<void> checkNickNameDuplication() async {
+    String nickName = nickNameController.text;
+
+    // .env에서 서버 주소 가져오기
+    final apiAddress = Uri.parse("${dotenv.get("API_ADDRESS")}/api/auth/duplication/nickName?nickName=$nickName");
+    final headers = {'Content-Type': 'application/json'};
+
+    try {
+      final response = await http.get(
+        apiAddress,
+        headers: headers
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        print("data: $data");
+        bool isDuplication = data["isDuplication"];
+
+        setState(() {
+          nickNameNotDuplication = !isDuplication;
+          isNickNameValid = !isDuplication;
+        });
+
+        if (nickNameNotDuplication) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("사용 가능한 닉네임입니다."))
+          );
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("중복된 닉네임입니다."))
+          );
+        }
+      } else {
+        log(response.body);
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("닉네임 중복 확인 실패"))
         );
       }
     } catch (e) {
@@ -130,8 +193,16 @@ class _RegisterScreenState extends State<RegisterScreen> {
                                   child: TextFormField(
                                     controller: idController,
                                     focusNode: idFocus,
+                                    autovalidateMode: AutovalidateMode.onUserInteraction,
                                     validator: (value) {
-                                      
+                                      return Checkvalidate().validateId(value, idNotDuplication);
+                                    },
+                                    onChanged: (value) {
+                                      setState(() {
+                                        isIdInput = Checkvalidate().checkIdInput(value);
+                                        idNotDuplication = false;
+                                        isIdValid = Checkvalidate().validateId(value, idNotDuplication) == null;
+                                      });
                                     },
                                     decoration: const InputDecoration(
                                       hintText: "아이디를 입력해주세요.",
@@ -156,9 +227,11 @@ class _RegisterScreenState extends State<RegisterScreen> {
                                 ),
                                 const SizedBox(width: 10),
                                 ElevatedButton(
-                                  onPressed: () {
-                                    checkIdDuplication();
-                                  },
+                                  onPressed: isIdInput
+                                  ? () {
+                                      checkIdDuplication();
+                                    }
+                                  : null,
                                   style: ElevatedButton.styleFrom(
                                     minimumSize: const Size(100, 50),
                                     backgroundColor: const Color.fromRGBO(122, 11, 11, 1) ,
@@ -195,6 +268,16 @@ class _RegisterScreenState extends State<RegisterScreen> {
                             TextFormField(
                               controller: passwordController,
                               focusNode: passwordFocus,
+                              obscureText: true,
+                              autovalidateMode: AutovalidateMode.onUserInteraction,
+                              validator: (value) {
+                                return Checkvalidate().validatePassword(value);
+                              },
+                              onChanged: (value) {
+                                setState(() {
+                                  isPasswordValid = Checkvalidate().validatePassword(passwordController.text) == null;  
+                                });
+                              },
                               decoration: const InputDecoration(
                                 hintText: "비밀번호를 입력해주세요.",
                                 enabledBorder: OutlineInputBorder(
@@ -233,6 +316,16 @@ class _RegisterScreenState extends State<RegisterScreen> {
                             TextFormField(
                               controller: rePasswordController,
                               focusNode: rePasswordFocus,
+                              obscureText: true,
+                              autovalidateMode: AutovalidateMode.onUserInteraction,
+                              validator: (value) {
+                                return Checkvalidate().validateRePassword(passwordController.text, value);
+                              },
+                              onChanged: (value) {
+                                setState(() {
+                                  isRePasswordValid = Checkvalidate().validateRePassword(passwordController.text, value) == null;  
+                                });
+                              },
                               decoration: const InputDecoration(
                                 hintText: "비밀번호를 다시 입력해주세요.",
                                 enabledBorder: OutlineInputBorder(
@@ -268,28 +361,68 @@ class _RegisterScreenState extends State<RegisterScreen> {
                                 fontWeight: FontWeight.bold
                               ),
                             ),
-                            TextFormField(
-                              controller: nickNameController,
-                              focusNode: nickNameFocus,
-                              decoration: const InputDecoration(
-                                hintText: "닉네임을 입력해주세요.",
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(5)
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: TextFormField(
+                                    controller: nickNameController,
+                                    focusNode: nickNameFocus,
+                                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                                    validator: (value) {
+                                      return Checkvalidate().validateNickName(value, nickNameNotDuplication);
+                                    },
+                                    onChanged: (value) {
+                                      setState(() {
+                                        isNickNameInput = nickNameController.text.trim().isNotEmpty;
+                                        nickNameNotDuplication = false;
+                                        isNickNameValid = Checkvalidate().validateNickName(value, nickNameNotDuplication) == null;
+                                      });
+                                    },
+                                    decoration: const InputDecoration(
+                                      hintText: "닉네임을 입력해주세요.",
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(5)
+                                        ),
+                                        borderSide: BorderSide(
+                                          color: Color.fromRGBO(121, 55, 64, 0.612)
+                                        )
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(5)
+                                        ),
+                                        borderSide: BorderSide(
+                                          color: Color.fromRGBO(122, 11, 11, 1)
+                                        )
+                                      )
+                                    ),
                                   ),
-                                  borderSide: BorderSide(
-                                    color: Color.fromRGBO(121, 55, 64, 0.612)
-                                  )
                                 ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(5)
+                                const SizedBox(width: 10),
+                                ElevatedButton(
+                                  onPressed: isNickNameInput
+                                  ? () {
+                                      checkNickNameDuplication();
+                                    }
+                                  : null,
+                                  style: ElevatedButton.styleFrom(
+                                    minimumSize: const Size(100, 50),
+                                    backgroundColor: const Color.fromRGBO(122, 11, 11, 1) ,
+                                    foregroundColor: Colors.white,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(5)
+                                    )
                                   ),
-                                  borderSide: BorderSide(
-                                    color: Color.fromRGBO(122, 11, 11, 1)
+                                  child: const Text(
+                                    "중복확인",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold
+                                    ),
                                   )
                                 )
-                              ),
+                              ],
                             )
                           ],
                         ),
@@ -299,9 +432,11 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 ),
               ),
               ElevatedButton(
-                onPressed: () {
-                  print("회원가입 버튼 클릭!!!");
-                },
+                onPressed: isIdValid && isPasswordValid && isRePasswordValid && isNickNameValid
+                ? () {
+                    print("회원가입 버튼 클릭!!!");
+                  }
+                : null,
                 style: ElevatedButton.styleFrom(
                   minimumSize: const Size.fromHeight(50),
                   backgroundColor: const Color.fromRGBO(122, 11, 11, 1),
