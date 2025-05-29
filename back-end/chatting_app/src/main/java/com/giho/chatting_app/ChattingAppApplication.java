5b8f6166f63714492f35cc06bcd43ff53299fd82
giho <ff6022@naver.com>
2025-05-29 22:56:39 +0900

    .env 파일에서 값을 가져올 수 있도록 Spring Boot의 main 파일 수정

--- back-end/chatting_app/src/main/java/com/giho/chatting_app/ChattingAppApplication.java
+++ back-end/chatting_app/src/main/java/com/giho/chatting_app/ChattingAppApplication.java
@@ -1,5 +1,6 @@
 package com.giho.chatting_app;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChattingAppApplication {
 
 	public static void main(String[] args) {
+
+		// .env 파일 로드
+		Dotenv dotenv = Dotenv.configure().load();
+
+		// MYSQL_PASSWORD를 환경 변수로 설정
+		System.setProperty("MONGODB_URL", dotenv.get("MONGODB_URL"));
+		System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
+		System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+		System.setProperty("JWT_ISSUER", dotenv.get("JWT_ISSUER"));
+		System.setProperty("ACCESS_TOKEN_EXP", dotenv.get("ACCESS_TOKEN_EXP"));
+		System.setProperty("REFRESH_TOKEN_EXP", dotenv.get("REFRESH_TOKEN_EXP"));
+		System.setProperty("PROFILE_IMAGE_PATH", dotenv.get("PROFILE_IMAGE_PATH"));
+
 		SpringApplication.run(ChattingAppApplication.class, args);
 	}
 
