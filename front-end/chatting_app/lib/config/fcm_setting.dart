f21669318347402e0fd4e40dd315382a44a3542e
giho <ff6022@naver.com>
2025-06-11 20:04:58 +0900

    어플이 실행되고 있을 때 알림이 안 오고 멈추는 오류 해결

--- front-end/chatting_app/lib/config/fcm_setting.dart
+++ front-end/chatting_app/lib/config/fcm_setting.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
+@pragma('vm:entry-point')
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
   print("Handling background message: ${message.messageId}");
